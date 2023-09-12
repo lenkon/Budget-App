80da1f26a54cd239f2f083d8d28c01882ab4b0c5
inoulen inoulen <inoulenkon@gmail.com>
2023-09-12 15:18:28 +0530

    Add foreign keys for the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_090721) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_094311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,11 +20,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_090721) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_bills_on_author_id"
+    t.index ["group_id"], name: "index_bills_on_group_id"
   end
 
   create_table "group_bills", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.bigint "bill_id", null: false
+    t.index ["bill_id"], name: "index_group_bills_on_bill_id"
+    t.index ["group_id"], name: "index_group_bills_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -33,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_090721) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_090721) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bills", "groups"
+  add_foreign_key "bills", "users", column: "author_id"
+  add_foreign_key "group_bills", "bills"
+  add_foreign_key "group_bills", "groups"
+  add_foreign_key "groups", "users"
 end
