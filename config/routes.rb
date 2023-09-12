f1cb8752ae10a20541df2e6f89263d08fab4a31a
inoulen inoulen <inoulenkon@gmail.com>
2023-09-12 21:21:34 +0530

    Add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'group_bills/index'
-  get 'group_bills/show'
-  get 'group_bills/new'
-  get 'group_bills/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
+  resources :users
   resources :groups do
     resources :bills
   end
