ae7fd88e33b7169a75c9a50b954ee4a5a2261e68
inoulen inoulen <inoulenkon@gmail.com>
2023-09-13 08:41:50 +0530

    Add bill model validations and tests

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,6 @@
 class Bill < ApplicationRecord
   belongs_to :user
+  
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 end
