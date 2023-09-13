6d93f4ed48f13c95f481a02a5870144ccc11e800
inoulen inoulen <inoulenkon@gmail.com>
2023-09-13 11:06:54 +0530

    Add bill and author foreign key association

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,6 @@
 class Bill < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
+  belongs_to :group
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
